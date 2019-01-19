6f97fd06b56ae6127d79a45db9b2b55c80800862
Prageeth Sarukara <spmonasha@gmail.com>
2019-01-20 01:04:55 +0530

    Updates

---
+++ precache-manifest.e8e30076fa44edaee03ae2570e63c725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mnsh-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "997434bdc727ea977112",
+    "url": "/mnsh-test/static/js/runtime~main.997434bd.js"
+  },
+  {
+    "revision": "e5ba1bb3f26d12d7b160",
+    "url": "/mnsh-test/static/js/main.e5ba1bb3.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/mnsh-test/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e5ba1bb3f26d12d7b160",
+    "url": "/mnsh-test/static/css/main.9518cf4c.chunk.css"
+  },
+  {
+    "revision": "72ba1115195861a4f43bf2e42f7738a0",
+    "url": "/mnsh-test/index.html"
+  }
+];
